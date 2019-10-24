3dd7d31a4eb12d99e1883995727c277b99dc2331
Irene Rojas <irojas@plan-sys.com>
2019-10-24 11:19:50 -0400

    added search to header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CreateLink from './components/CreateLink'
 import Header from './components/Header'
 import { Switch, Route } from 'react-router-dom'
 import Login from './components/Login'
+import Search from './components/Search'
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
             <Route exact path="/login" component={Login} />
+            <Route exact path='/search' component={Search} />
           </Switch>
         </div>
       </div>
